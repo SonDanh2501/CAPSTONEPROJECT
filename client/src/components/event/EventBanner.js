d15ff869a7e749acfa97426d1494b05818f06ca9
SonDanh2501 <sondanhwork2501@gmail.com>
2024-06-09 13:03:48 +0700

    commit

--- client/src/components/event/EventBanner.js
+++ client/src/components/event/EventBanner.js
@@ -2,39 +2,19 @@ import React from "react";
 import ball from "../../assets/bg-discount.jpg";
 import icons from "ultils/icons";
 import { useTranslation } from "react-i18next";
+import backgroundImage from "assets/event_banner.jpg";
 
-const { FaArrowRight } = icons;
+const { IoArrowForwardOutline } = icons;
 const EventBanner = () => {
   const { t } = useTranslation();
-  const { event1, event2, event3, event4, event5 } = t("eventbanner")
+  const { event1, event2, event3, event4, event5 } = t("eventbanner");
   return (
-    <div className="w-full h-[553px] bg-bg-light flex items-center justify-center dark:bg-medium">
-      <div className="flex flex-col md:flex-row">
-        <div className="flex flex-col items-center md:items-start pt-8 px-4 gap-2 w-">
-          <img src={ball} alt="" className="w-full h-[300px] object-cover" />
-        </div>
-
-        <div className="px-4 mt-4">
-          <div className="flex flex-col">
-            <h1 className="text-4xl font-semibold py-2 md:py-5">
-              <span className="text-indigo-500 font-bold">{event1}</span>
-            </h1>
-            <p className="text-sm md:text-base dark:text-white">
-              {event2}
-            </p>
-            <div className="flex items-center gap-2 mt-2">
-              <span className="text-lg text-black dark:text-white">{event3}</span>
-              <span className="text-4xl text-yellow-400">50%</span>
-              <span className="text-lg text-black dark:text-white">{event4}</span>
-            </div>
-            <button className="text-white mt-6 flex flex-grow items-center justify-center gap-2 px-2 py-2 rounded-3xl relative overflow-hidden bg-gray-800 shadow-lg transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-blue-500 before:transition-all before:duration-500 hover:text-white hover:shadow-blue-400 hover:before:left-0 hover:before:w-full">
-              <span className="relative">{event5}</span>
-              <span className="relative">
-                <FaArrowRight />
-              </span>
-            </button>
-          </div>
-        </div>
+    <div className="w-full flex items-center justify-center py-8">
+      {/*Image Background*/}
+      <div className="w-[90vw] flex justify-end bg-gray-500 p-12">
+        <img src={backgroundImage}></img>
+        {/*Content*/}
+      
       </div>
     </div>
   );
